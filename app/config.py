d01305558fa3998cfd2f7eec8250e0dd4788bc46
Michael B <mbrotmans@gmail.com>
2021-02-16 08:46:31 -0500

    initial commit

---
+++ app/config.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2018 Amir Hadifar. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+# Generate API_KEY and API_SECRET from ==> https://apps.twitter.com
+API_KEY = "API_KEY_REPLACEME"
+API_SECRET = "API_SECRET_REPLACEME"
+
+# In Keys and access tokens click on generate access token button
+ACCESS_TOKEN = "ACCESS_TOKEN_REPLACEME"
+ACCESS_TOKEN_SECRET = "ACCESS_TOKEN_SECRET_REPLACEME"
+
+# channel name and Bot API key ==> https://core.telegram.org/bots/api
+TELEGRAM_CHANNEL_NAME = "TELEGRAM_CHANNEL_NAME_REPLACEME"
+TELEGRAM_BOT_API_KEY = "TELEGRAM_BOT_API_KEY_REPLACEME"
---
+++ app/listener.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2018 Amir Hadifar. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import json
+
+import requests
+import time
+from tweepy import StreamListener
+from sys import stderr
+from app import config
+
+
+class Listener(StreamListener):
+    ENDPOINT = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}"
+
+    def on_data(self, data):
+        all_data = json.loads(data)
+        print(all_data)
+        try:
+            text = all_data['text']
+            user_id = all_data["user"]["screen_name"]
+            tweet_id = all_data["id_str"]
+            if not text.startswith("RT"):
+                tweet =  "https://twitter.com/" + user_id + "/status/" + tweet_id
+                req = self.ENDPOINT.format(config.TELEGRAM_BOT_API_KEY, config.TELEGRAM_CHANNEL_NAME, tweet)
+                requests.get(req)
+        except:
+            pass
+
+
+        return True
+
+    def on_disconnect(self, notice):
+        """Called when twitter sends a disconnect notice
+
+        Disconnect codes are listed here:
+        https://dev.twitter.com/docs/streaming-apis/messages#Disconnect_messages_disconnect
+        """
+        return
+
+
+    def on_timeout(self):
+        # Print timeout message
+        print(stderr, "Timeout...")
+
+        # Wait 10 seconds
+        time.sleep(10)
+
+        # Return nothing
+        return
+
+
+    def on_error(self, status):
+        print ('error with status code' + str(status))
